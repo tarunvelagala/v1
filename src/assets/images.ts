bd3345871069d999e400c017060323bf6f345671
Tarun Velagala <tarunvelagala80@gmail.com>
2025-03-18 14:56:38 +0000

    Modify profile image

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -4,7 +4,7 @@
 
 export const images = {
   // Placeholder for profile image
-  profile: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
+  profile: './profile_pic.jpg',
   
   // Company logos
   companyLogos: {
