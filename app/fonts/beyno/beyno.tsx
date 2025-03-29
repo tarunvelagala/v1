f75a94381410716e7825fcbbdfde4527686765ac
Tarun Velagala <tarunvelagala80@gmail.com>
2025-03-29 12:38:32 +0000

    Adds Beyno font for navbar icon

---
+++ app/fonts/beyno/beyno.tsx
@@ -0,0 +1,5 @@
+import localFont from 'next/font/local'
+
+export const beynoFont = localFont({
+    src: './beyno.ttf'
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { ThemeToggle } from "@/components/theme-toggle"
+import { beynoFont } from "@/app/fonts/beyno/beyno"
 
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <div className={beynoFont.className}>
+        TV
+      </div>
       <ThemeToggle />
     </main>
   );
